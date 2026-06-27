1ac7d0de363087cc84265d689c0f039f4235c9b3
bc.lucena <bc.lucena@gmail.com>
2026-06-26 21:38:09 -0300

    fix: outputFileTracingRoot ESM correto

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -10,7 +16,7 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
   },
-  outputFileTracingRoot: require('path').join(__dirname, '../../'),
+  outputFileTracingRoot: join(__dirname, '../../'),
 }
 
 export default nextConfig
